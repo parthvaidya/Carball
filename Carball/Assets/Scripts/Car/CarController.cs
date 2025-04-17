4abf42099065f3ef29d6630516efd1970474ca9f
Parth Vaidya <zeusparth99@gmail.com>
2025-04-17 19:43:53 +0530

    controller updated

--- Carball/Assets/Scripts/Car/CarController.cs
+++ Carball/Assets/Scripts/Car/CarController.cs
@@ -1,25 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class CarController : MonoBehaviour
 {
-
+    private Rigidbody rb;
     public WheelColliders colliders;
     public WheelMeshes wheelmesh;
-
+    //public WheelParticles wheelParticles;
+    public float gasInput;
+    public float breakInput;
+    public float steeringInput;
+    
+    public float motorPower;
+    public float brakePower;
+    public float nitroBoost = 2000f;
+    //public float breakSpeed;
+    private float slipAngle;
+    private float speed;
+    public AnimationCurve steeringCurve;
     
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        speed = rb.velocity.magnitude;
+        CheckInput();
+        ApplyMotor();
+        ApplySteering();
+        ApplyBrake();
         ApplyWheelPositions();
+    }
+
+    void CheckInput()
+    {
+        gasInput = Input.GetAxis("Vertical");
+        steeringInput = Input.GetAxis("Horizontal");
+        slipAngle = Vector3.Angle(transform.forward, rb.velocity - transform.forward);
+        if(slipAngle < 120f)
+        {
+            if(gasInput < 0 && Vector3.Dot(transform.forward, rb.velocity) > 0)
+            {
+                breakInput = Mathf.Abs(gasInput);
+                gasInput = 0;
+            }
+            
+            
+        }
+        else
+        {
+            breakInput = 0;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            rb.AddForce(transform.forward * nitroBoost, ForceMode.Acceleration);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            breakInput = 1f; // Max brake
+        }
+    }
+
+    void ApplyBrake()
+    {
+        float brakeForce = breakInput * brakePower;
+        colliders.FrontRightWheel.brakeTorque = brakeForce * 0.7f;
+        colliders.FrontLeftWheel.brakeTorque = brakeForce * 0.7f;
+        colliders.RearLeftWheel.brakeTorque = brakeForce * 0.3f;
+        colliders.RearRightWheel.brakeTorque = brakeForce * 0.3f;
+    }
+
+    void ApplyMotor()
+    {
+        colliders.RearRightWheel.motorTorque = motorPower * gasInput;
+        colliders.RearLeftWheel.motorTorque = motorPower * gasInput;
     }
     void ApplyWheelPositions()
     {
@@ -27,6 +90,31 @@ public class CarController : MonoBehaviour
         UpdateWheel(colliders.FrontLeftWheel, wheelmesh.FrontLeftWheel);
         UpdateWheel(colliders.RearLeftWheel, wheelmesh.RearLeftWheel);
         UpdateWheel(colliders.RearRightWheel, wheelmesh.RearRightWheel);
+    }
+
+    //void ApplySteering()
+    //{
+    //    float steeringAngle = steeringInput * steeringCurve.Evaluate(speed);
+    //    steeringAngle += Vector3.SignedAngle(transform.forward, rb.velocity, Vector3.up);
+    //    steeringAngle = Mathf.Clamp(steeringAngle, -90f, 90f);
+    //    colliders.FrontRightWheel.steerAngle = steeringAngle;
+    //    colliders.FrontLeftWheel.steerAngle = steeringAngle;
+    //}
+
+    void ApplySteering()
+    {
+        // Detect whether the car is moving forward or in reverse
+        float direction = Vector3.Dot(transform.forward, rb.velocity) >= 0 ? 1f : -1f;
+
+        float maxSteeringAngle = 30f; // Max steering angle in degrees
+        float steeringAngle = steeringInput * maxSteeringAngle * direction;
+
+        // Clamp for safety
+        steeringAngle = Mathf.Clamp(steeringAngle, -maxSteeringAngle, maxSteeringAngle);
+
+        // Apply to front wheels
+        colliders.FrontLeftWheel.steerAngle = steeringAngle;
+        colliders.FrontRightWheel.steerAngle = steeringAngle;
     }
 
     void UpdateWheel(WheelCollider coll , MeshRenderer wheelmesh)
@@ -58,5 +146,13 @@ public class CarController : MonoBehaviour
 
     }
 
+
+    //public class  WheelParticles 
+    //{
+    //    public ParticleSystem FrontLeftWheel;
+    //    public ParticleSystem FrontRightWheel;
+    //    public ParticleSystem RearLeftWheel;
+    //    public ParticleSystem RearRightWheel;
+    //}
 
 }
