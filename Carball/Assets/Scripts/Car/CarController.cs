08d55f429b08a9f22170cdb681fb90931e3bfc14
Parth Vaidya <zeusparth99@gmail.com>
2025-04-18 18:46:26 +0530

    Added invisible walls

--- Carball/Assets/Scripts/Car/CarController.cs
+++ Carball/Assets/Scripts/Car/CarController.cs
@@ -1,150 +1,227 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class CarController : MonoBehaviour
 {
-    private Rigidbody rb;
-    public WheelColliders colliders;
-    public WheelMeshes wheelmesh;
-    //public WheelParticles wheelParticles;
-    public float gasInput;
-    public float breakInput;
-    public float steeringInput;
+    public enum ControlMode
+    {
+        Keyboard,
+        Buttons
+    };
+
+    public enum Axel
+    {
+        Front,
+        Rear
+    }
+
+    [Serializable]
+    public struct Wheel
+    {
+        public GameObject wheelModel;
+        public WheelCollider wheelCollider;
+        //public GameObject wheelEffectObj;
+        //public ParticleSystem smokeParticle;
+        public Axel axel;
+    }
+
+    public ControlMode control;
+
+    public float maxAcceleration = 30.0f;
+    public float brakeAcceleration = 50.0f;
+
+    public float turnSensitivity = 1.0f;
+    public float maxSteerAngle = 30.0f;
+
+    public Vector3 _centerOfMass;
+
+    public List<Wheel> wheels;
+
+    float moveInput;
+    float steerInput;
+
+    private Rigidbody carRb;
+    public float nitroForce = 15000f;  // Multiplies acceleration when using nitro
+    private bool isNitro = false;
+    private bool isBraking = false;
     
-    public float motorPower;
-    public float brakePower;
-    public float nitroBoost = 2000f;
-    //public float breakSpeed;
-    private float slipAngle;
-    private float speed;
-    public AnimationCurve steeringCurve;
-    
-    
-    // Start is called before the first frame update
+
+
+
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody>();
+        carRb = GetComponent<Rigidbody>();
+        carRb.centerOfMass = _centerOfMass;
+
+        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        speed = rb.velocity.magnitude;
-        CheckInput();
-        ApplyMotor();
-        ApplySteering();
-        ApplyBrake();
-        ApplyWheelPositions();
+        GetInputs();
+        AnimateWheels();
+        //WheelEffects();
     }
 
-    void CheckInput()
+    void LateUpdate()
     {
-        gasInput = Input.GetAxis("Vertical");
-        steeringInput = Input.GetAxis("Horizontal");
-        slipAngle = Vector3.Angle(transform.forward, rb.velocity - transform.forward);
-        if(slipAngle < 120f)
+        Move();
+        Steer();
+        Brake();
+        NitroBoost();
+    }
+
+    public void MoveInput(float input)
+    {
+        moveInput = input;
+    }
+
+    public void SteerInput(float input)
+    {
+        steerInput = input;
+    }
+
+    //void GetInputs()
+    //{
+    //    if (control == ControlMode.Keyboard)
+    //    {
+    //        moveInput = Input.GetAxis("Vertical");
+    //        steerInput = Input.GetAxis("Horizontal");
+    //    }
+    //}
+
+    void GetInputs()
+    {
+        if (control == ControlMode.Keyboard)
         {
-            if(gasInput < 0 && Vector3.Dot(transform.forward, rb.velocity) > 0)
+            isNitro = Input.GetKeyDown(KeyCode.LeftShift);
+            isBraking = Input.GetKey(KeyCode.Space);
+
+            if (Input.GetKey(KeyCode.W))
             {
-                breakInput = Mathf.Abs(gasInput);
-                gasInput = 0;
+                moveInput = 1f;
             }
-            
-            
+            else if (Input.GetKey(KeyCode.S))
+            {
+                moveInput = -1f;
+            }
+            else
+            {
+                moveInput = 0f;
+            }
+
+            steerInput = Input.GetAxis("Horizontal");
+        }
+    }
+
+    //void Move()
+    //{
+    //    foreach (var wheel in wheels)
+    //    {
+    //        wheel.wheelCollider.motorTorque = moveInput * 600 * maxAcceleration * Time.deltaTime;
+    //    }
+    //}
+
+
+    void Move()
+    {
+        foreach (var wheel in wheels)
+        {
+            //wheel.wheelCollider.motorTorque = moveInput * 6000 * maxAcceleration * Time.deltaTime;
+            wheel.wheelCollider.motorTorque = moveInput * 600 * maxAcceleration;
+        }
+    }
+
+    void NitroBoost()
+    {
+        if (isNitro && moveInput > 0)
+        {
+            carRb.AddForce(transform.forward * nitroForce, ForceMode.Impulse);
+            isNitro = false; // Reset to avoid repeated triggers
+        }
+    }
+    void Steer()
+    {
+        foreach (var wheel in wheels)
+        {
+            if (wheel.axel == Axel.Front)
+            {
+                var _steerAngle = steerInput * turnSensitivity * maxSteerAngle;
+                wheel.wheelCollider.steerAngle = Mathf.Lerp(wheel.wheelCollider.steerAngle, _steerAngle, 0.6f);
+            }
+        }
+    }
+
+    //void Brake()
+    //{
+    //    if (Input.GetKey(KeyCode.Space) || moveInput == 0)
+    //    {
+    //        foreach (var wheel in wheels)
+    //        {
+    //            wheel.wheelCollider.brakeTorque = 300 * brakeAcceleration * Time.deltaTime;
+    //        }
+
+
+    //    }
+    //    else
+    //    {
+    //        foreach (var wheel in wheels)
+    //        {
+    //            wheel.wheelCollider.brakeTorque = 0;
+    //        }
+
+
+    //    }
+    //}
+
+
+    void Brake()
+    {
+        if (isBraking || moveInput == 0)
+        {
+            foreach (var wheel in wheels)
+            {
+                wheel.wheelCollider.brakeTorque = 30000 * brakeAcceleration * Time.deltaTime;
+            }
         }
         else
         {
-            breakInput = 0;
+            foreach (var wheel in wheels)
+            {
+                wheel.wheelCollider.brakeTorque = 0;
+            }
         }
+    }
 
-        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+    void AnimateWheels()
+    {
+        foreach (var wheel in wheels)
         {
-            rb.AddForce(transform.forward * nitroBoost, ForceMode.Acceleration);
+            Quaternion rot;
+            Vector3 pos;
+            wheel.wheelCollider.GetWorldPose(out pos, out rot);
+            wheel.wheelModel.transform.position = pos;
+            wheel.wheelModel.transform.rotation = rot;
         }
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            breakInput = 1f; // Max brake
-        }
     }
 
-    void ApplyBrake()
-    {
-        float brakeForce = breakInput * brakePower;
-        colliders.FrontRightWheel.brakeTorque = brakeForce * 0.7f;
-        colliders.FrontLeftWheel.brakeTorque = brakeForce * 0.7f;
-        colliders.RearLeftWheel.brakeTorque = brakeForce * 0.3f;
-        colliders.RearRightWheel.brakeTorque = brakeForce * 0.3f;
-    }
-
-    void ApplyMotor()
-    {
-        colliders.RearRightWheel.motorTorque = motorPower * gasInput;
-        colliders.RearLeftWheel.motorTorque = motorPower * gasInput;
-    }
-    void ApplyWheelPositions()
-    {
-        UpdateWheel(colliders.FrontRightWheel, wheelmesh.FrontRightWheel);
-        UpdateWheel(colliders.FrontLeftWheel, wheelmesh.FrontLeftWheel);
-        UpdateWheel(colliders.RearLeftWheel, wheelmesh.RearLeftWheel);
-        UpdateWheel(colliders.RearRightWheel, wheelmesh.RearRightWheel);
-    }
-
-    
-    void ApplySteering()
-    {
-        // Detect whether the car is moving forward or in reverse
-        float direction = Vector3.Dot(transform.forward, rb.velocity) >= 0 ? 1f : -1f;
-
-        float maxSteeringAngle = 30f; // Max steering angle in degrees
-        float steeringAngle = steeringInput * maxSteeringAngle * direction;
-
-        // Clamp for safety
-        steeringAngle = Mathf.Clamp(steeringAngle, -maxSteeringAngle, maxSteeringAngle);
-
-        // Apply to front wheels
-        colliders.FrontLeftWheel.steerAngle = steeringAngle;
-        colliders.FrontRightWheel.steerAngle = steeringAngle;
-    }
-
-    void UpdateWheel(WheelCollider coll , MeshRenderer wheelmesh)
-    {
-        Quaternion quat;
-        Vector3 position;
-        coll.GetWorldPose(out position, out quat);
-        wheelmesh.transform.position = position;
-        wheelmesh.transform.rotation = quat;
-    }
-
-    [System.Serializable]
-    public class WheelColliders
-    {
-        public WheelCollider FrontLeftWheel;
-        public WheelCollider FrontRightWheel;
-        public WheelCollider RearLeftWheel;
-        public WheelCollider RearRightWheel;
-
-    }
-
-    [System.Serializable]
-    public class WheelMeshes
-    {
-        public MeshRenderer FrontLeftWheel;
-        public MeshRenderer FrontRightWheel;
-        public MeshRenderer RearLeftWheel;
-        public MeshRenderer RearRightWheel;
-
-    }
-
-
-    //public class  WheelParticles 
+    //void WheelEffects()
     //{
-    //    public ParticleSystem FrontLeftWheel;
-    //    public ParticleSystem FrontRightWheel;
-    //    public ParticleSystem RearLeftWheel;
-    //    public ParticleSystem RearRightWheel;
-    //}
+    //    foreach (var wheel in wheels)
+    //    {
+    //        //var dirtParticleMainSettings = wheel.smokeParticle.main;
 
+    //        if (Input.GetKey(KeyCode.Space) && wheel.axel == Axel.Rear && wheel.wheelCollider.isGrounded == true && carRb.velocity.magnitude >= 10.0f)
+    //        {
+    //            wheel.wheelEffectObj.GetComponentInChildren<TrailRenderer>().emitting = true;
+    //            wheel.smokeParticle.Emit(1);
+    //        }
+    //        else
+    //        {
+    //            wheel.wheelEffectObj.GetComponentInChildren<TrailRenderer>().emitting = false;
+    //        }
+    //    }
+    //}
 }
